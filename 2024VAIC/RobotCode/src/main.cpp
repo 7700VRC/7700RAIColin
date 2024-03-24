e012573a5da6f92574bb4226cf1af019d48ee0a5
ColColA <71855826+ColColA@users.noreply.github.com>
2024-03-24 13:57:49 -0700

    added my code

---
+++ 2024VAIC/RobotCode/src/main.cpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       homunculus                                                */
+/*    Created:      3/24/2024, 1:30:59 PM                                     */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of vex::brain used for printing to the V5 brain screen
+brain Brain;
+
+// define your global instances of motors and other devices here
+
+
+int main() {
+   
+}
